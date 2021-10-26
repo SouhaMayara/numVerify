5cae9a23fd60519d3a449fb504ce3832adbbd689
Souha <mayara.souha@gmail.com>
2021-10-26 19:02:57 +0100

    'update'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-// import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
-// import { MatDatepickerModule } from '@angular/material/datepicker';
-// import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
-// import { MatListModule} from '@angular/material/list';
-
-// import {Ng2TelInputModule} from 'ng2-tel-input';
-// import {NgxMatIntlTelInputModule} from 'ngx-mat-intl-tel-input';
-
 import {HttpClientModule} from'@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
@@ -34,12 +25,6 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatSelectModule,
     MatCardModule,
     MatButtonModule,
-    // MatRadioModule,
-    // MatDatepickerModule,
-    // MatNativeDateModule,
-    // MatListModule,
-    // Ng2TelInputModule,
-    // NgxMatIntlTelInputModule,
     HttpClientModule,
     MatSnackBarModule
   ],
